2d357a74073233f33f967bb239c11f1876b48fd4
Joshua Bradbury <Joshua.Bradbury@protonmail.com>
2018-04-23 15:34:22 +0100

    Added some more tests

--- src/test/kotlin/uk/co/newagedev/knade/math/VectorTest.kt
+++ src/test/kotlin/uk/co/newagedev/knade/math/VectorTest.kt
@@ -1,7 +1,6 @@
 package uk.co.newagedev.knade.math
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.*
 
 class VectorTest {
 
@@ -11,6 +10,12 @@ class VectorTest {
 
         assertEquals(1.5f, vec.x)
         assertEquals(-2.5f, vec.y)
+
+        vec.x = 2.5f
+        vec.y = -5f
+
+        assertEquals(2.5f, vec.x)
+        assertEquals(-5f, vec.y)
     }
 
     @Test
@@ -20,6 +25,14 @@ class VectorTest {
         assertEquals(1.5f, vec.x)
         assertEquals(-2.5f, vec.y)
         assertEquals(1.2f, vec.z)
+
+        vec.x = 2.5f
+        vec.y = -5f
+        vec.z = 75.16f
+
+        assertEquals(2.5f, vec.x)
+        assertEquals(-5f, vec.y)
+        assertEquals(75.16f, vec.z)
     }
 
     @Test
@@ -48,6 +61,16 @@ class VectorTest {
         assertEquals(-2.5f, vec.y)
         assertEquals(1.2f, vec.z)
         assertEquals(-5.3f, vec.w)
+
+        vec.x = 2.5f
+        vec.y = -5f
+        vec.z = 75.16f
+        vec.w = -24.65f
+
+        assertEquals(2.5f, vec.x)
+        assertEquals(-5f, vec.y)
+        assertEquals(75.16f, vec.z)
+        assertEquals(-24.65f, vec.w)
     }
 
     @Test
@@ -152,5 +175,107 @@ class VectorTest {
         assertEquals(-2.5f, vec.t)
         assertEquals(1.2f, vec.p)
         assertEquals(-5.3f, vec.q)
+    }
+
+    @Test
+    fun vec2fEqualsVec2f() {
+        val vec = Vec2f(1.0f)
+        val vec2 = Vec2f(2.0f)
+        val vec3 = Vec2f(1.0f, 1.0f)
+        val vec4: Any = Vec4f(1.0f)
+        val vec5 = Vec2f(1.0f, 2.0f)
+
+        assertTrue(vec == vec)
+        assertFalse(vec == vec2)
+        assertTrue(vec == vec3)
+        assertFalse(vec == vec4)
+        assertFalse(vec == vec5)
+
+        assertNotNull(vec.hashCode())
+
+        assertEquals(vec.x, vec.component1())
+        assertEquals(vec.y, vec.component2())
+    }
+
+    @Test
+    fun vec3fEqualsVec3f() {
+        val vec = Vec3f(1.0f)
+        val vec2 = Vec3f(2.0f)
+        val vec3 = Vec3f(1.0f, 1.0f, 1.0f)
+        val vec4: Any = Vec4f(1.0f)
+        val vec5 = Vec3f(1.0f, 2.0f, 1.0f)
+        val vec6 = Vec3f(1.0f, 1.0f, 2.0f)
+
+        assertTrue(vec == vec)
+        assertFalse(vec == vec2)
+        assertTrue(vec == vec3)
+        assertFalse(vec == vec4)
+        assertFalse(vec == vec5)
+        assertFalse(vec == vec6)
+
+        assertNotNull(vec.hashCode())
+
+        assertEquals(vec.x, vec.component1())
+        assertEquals(vec.y, vec.component2())
+        assertEquals(vec.z, vec.component3())
+    }
+
+    @Test
+    fun vec4fEqualsVec4f() {
+        val vec = Vec4f(1.0f)
+        val vec2 = Vec4f(2.0f)
+        val vec3 = Vec4f(1.0f, 1.0f, 1.0f, 1.0f)
+        val vec4: Any = Vec2f(1.0f)
+        val vec5 = Vec4f(1.0f, 2.0f, 1.0f, 1.0f)
+        val vec6 = Vec4f(1.0f, 1.0f, 2.0f, 1.0f)
+        val vec7 = Vec4f(1.0f, 1.0f, 1.0f, 2.0f)
+
+        assertTrue(vec == vec)
+        assertFalse(vec == vec2)
+        assertTrue(vec == vec3)
+        assertFalse(vec == vec4)
+        assertFalse(vec == vec5)
+        assertFalse(vec == vec6)
+        assertFalse(vec == vec7)
+
+        assertNotNull(vec.hashCode())
+
+        assertEquals(vec.x, vec.component1())
+        assertEquals(vec.y, vec.component2())
+        assertEquals(vec.z, vec.component3())
+        assertEquals(vec.z, vec.component4())
+    }
+
+    @Test
+    fun vec2fToString() {
+        assertEquals("Vec2f(x=1.0, y=1.0)", Vec2f(1.0f).toString())
+    }
+
+    @Test
+    fun vec3fToString() {
+        assertEquals("Vec3f(x=1.0, y=1.0, z=1.0)", Vec3f(1.0f).toString())
+    }
+
+    @Test
+    fun vec4fToString() {
+        assertEquals("Vec4f(x=1.0, y=1.0, z=1.0, w=1.0)", Vec4f(1.0f).toString())
+    }
+
+    @Test
+    fun vec2fCopy() {
+        val vec = Vec2f(1.0f)
+        assertEquals(vec.copy(), vec)
+    }
+
+    @Test
+    fun vec3fCopy() {
+        val vec = Vec3f(1.0f)
+        assertEquals(vec.copy(), vec)
+    }
+
+    @Test
+    fun vec4fCopy() {
+        val vec = Vec4f(1.0f)
+        assertEquals(vec.copy(), vec)
     }
 }
